dc14a0ac26396bb656e20de8d6bcc879a100df8a
fcsm-andrei <andreileonchikov@gmail.com>
2018-09-22 23:01:12 +0300

    sort tasttask sort

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+      constructor() {
+        this.arr = [];
+        this.compareFunction = function(a, b) {
+            return a - b;
+          }    
+      }
 
-  add(element) {
-    // your implementation
-  }
+      add(element) {
+        return this.arr.push(element);
+      }
 
-  at(index) {
-    // your implementation
-  }
+      at(index) {
+        return this.arr[index];
+      }
 
-  get length() {
-    // your implementation
-  }
+      get length() {
+        return this.arr.length
+      }
 
-  toArray() {
-    // your implementation
-  }
+      toArray() {
+        return this.arr;
+      }
 
-  sort(indices) {
-    // your implementation
-  }
+      sort(indices) {
+        var addSort = []; 
+        var num = indices.length-1;  
+        for (var i = num;  i >= 0; i--) {
+          addSort.push(this.arr[indices[i]]);
+        }        
+        
+        addSort.sort(this.compareFunction);
+        indices.sort();
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        for (var i = num;  i >= 0; i--) {
+          this.arr[indices[i]] = addSort[i];
+        }
+
+        return this.arr;
+      }
+
+      setComparator(compareFunction) {
+        return this.compareFunction = compareFunction;
+      }
 }
 
 module.exports = Sorter;
